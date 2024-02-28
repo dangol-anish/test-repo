6261d065ed47be8c81b72308c10168ccfadbb5f9
Anish Dangol <dangol.anish001@gmail.com>
2024-02-28 21:55:20 +0545

    socket and chat both working till now

--- app/app.js
+++ app/app.js
@@ -1,20 +1,15 @@
+const socket = io("ws://192.168.0.102:8080");
 
-const socket = io('ws://localhost:8080');
-
-socket.on('message', text => {
-
-    const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el)
-
+socket.on("message", (text) => {
+  const el = document.createElement("li");
+  el.innerHTML = text;
+  document.querySelector("ul").appendChild(el);
 });
 
-document.querySelector('button').onclick = () => {
-
-    const text = document.querySelector('input').value;
-    socket.emit('message', text)
-    
-}
+document.querySelector("button").onclick = () => {
+  const text = document.querySelector("input").value;
+  socket.emit("message", text);
+};
 
 // Regular Websockets
 
@@ -28,3 +23,32 @@ document.querySelector('button').onclick = () => {
 // document.querySelector('button').onclick = () => {
 //     socket.send('hello');
 // }
+
+let loginForm = document.getElementById("myForm");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  const formData = {
+    username,
+    password,
+  };
+
+  try {
+    const res = await fetch("http://192.168.0.102:3000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
--- server/index.js
+++ server/index.js
@@ -1,28 +1,26 @@
+const http = require("http").createServer();
 
-const http = require('http').createServer();
-
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.on('message', (message) =>     {
-        console.log(message);
-        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
-    });
+  socket.on("message", (message) => {
+    console.log(message);
+    io.emit("message", `${socket.id.substr(0, 2)} said ${message}`);
+  });
 });
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
-
+http.listen(8080, () => console.log("listening on http://192.168.0.102:8080"));
 
 // Regular Websockets
 
 // const WebSocket = require('ws')
 // const server = new WebSocket.Server({ port: '8080' })
 
-// server.on('connection', socket => { 
+// server.on('connection', socket => {
 
 //   socket.on('message', message => {
 
@@ -32,5 +30,37 @@ http.listen(8080, () => console.log('listening on http://localhost:8080') );
 
 // });
 
+const express = require("express");
+const cors = require("cors");
 
- 
+const app = express();
+
+const mysqli = require("mysql");
+
+app.use(express.json());
+app.use(cors());
+
+const con = mysqli.createConnection({
+  host: "192.168.0.102",
+  user: "root",
+  password: "",
+  database: "ctf_test",
+});
+
+app.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  const insertQuery = `insert into test values ('${username}','${password}')`;
+
+  con.query(insertQuery, (err, result) => {
+    if (err) throw new err();
+    console.log(result);
+  });
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
+});
+
+app.listen(3000, "192.168.0.102");
